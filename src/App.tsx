bac1394687417a14ac2494a0f4f2f34cab8e5d6e
kwakhyun <khyun2348@naver.com>
2022-12-06 10:28:00 +0900

    Feat: initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { QuizProvider } from './context/QuizContext';
+import { Global, ThemeProvider } from '@emotion/react';
+import theme from './styles/theme';
+import { global } from './styles';
+import Router from './router';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QuizProvider>
+      <ThemeProvider theme={theme}>
+        <Global styles={global} />
+        <Router />
+      </ThemeProvider>
+    </QuizProvider>
   );
-}
+};
 
 export default App;
